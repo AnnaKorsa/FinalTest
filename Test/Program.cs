2a59c135e6d2dc089e4729b19e3d4b6cbab277b7
Anna Kors <ankors320@gmail.com>
2023-04-30 17:02:29 +0300

    Input initial array

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(string[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"Введите {i + 1} элемент массива: ");
+		array[i] = Console.ReadLine()!;
+	}
+}
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n <= 0)
+{
+	Console.Write("Вы ошиблись!\nВведите количество элементов массива больше 0: ");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+string[] array = new string[n];
+InputArray(array);
+Console.WriteLine();
+Console.Write($"Первоначальный массив: ");
